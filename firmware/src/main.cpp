78852698aafe7ae66c5c12b02ac6432fcc059c42
melektron <matteo@elektron.work>
2023-06-28 15:39:30 +0200

    added blink

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED 23
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+    pinMode(LED, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    digitalWrite(LED, 1);
+    delay(1000);
+    digitalWrite(LED, 0);
+    delay(1000);
 }
