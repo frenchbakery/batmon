cbf7e62dcba908119f7e751b1a12c1c2c4cdba32
melektron <matteo@elektron.work>
2023-07-02 14:39:44 +0200

    new pinouts

--- test_firmware/src/main.cpp
+++ test_firmware/src/main.cpp
@@ -4,15 +4,18 @@
 
 #define BUZZER 14
 #define LED 26
-#define C1I 16
-#define C2I 17
+#define C1I 35
+#define C2I 34
 
 
 void setup()
 {
     // setup diGital pins
     pinMode(BUZZER, OUTPUT);
+    digitalWrite(BUZZER, 0);
     pinMode(LED, OUTPUT);
+    pinMode(C1I, INPUT);
+    pinMode(C2I, INPUT);
 }
 
 
@@ -24,14 +27,14 @@ void loop()
     UC1 = analogRead(C1I);
     UC2 = analogRead(C2I);
 
-    printf("ADC values: %d\t%d", UC1, UC2);
+    printf("ADC values: %d\t%d\n", UC1, UC2);
 
     // "blink" LED and Buzzer
     digitalWrite(LED, 1);
-    tone(BUZZER, 440);
+    //tone(BUZZER, 440);
 
     delay(500);
     digitalWrite(LED, 0); 
-    noTone(BUZZER);
+    //noTone(BUZZER);
     delay(500);
 }
